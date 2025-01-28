802cc917134010f60b8bfb70a17197dd4606b3e6
zooniaffreene <zooniaffreene1073@gmail.com>
2025-01-27 22:43:27 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,21 @@
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args)
-    {
-        // Task:
-        // 1. Implement missing sorting algorithms.
-        //    - InsertionSort.java
-        //    - SelectionSort.java
-        //
-        //  2. Add a method to class Main to generate a random array.
-        //  3. Provide random input to test suite and ensure your implementations
-        //     of InsertionSort and Selection sort continue to work.
-        //  4. Increase the loop count for TestSuite to get better performance results.
+    public static void main(String[] args) {
+        // Generate a random array
+        int[] testInput = generateRandomArray(10, 0, 100);
 
-        int[] testInput = { 1, 2, 3, 1 };
+        TestSuite.run(testInput, 10000);
+    }
 
-        // Increase the loop count to get better results once it works.
-        // TestSuite.run(testInput, 10000);
-        TestSuite.run(testInput, 1);
+    public static int[] generateRandomArray(int size, int minValue, int maxValue) {
+        Random random = new Random();
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(maxValue - minValue + 1) + minValue;
+        }
+
+        return array;
     }
 }
