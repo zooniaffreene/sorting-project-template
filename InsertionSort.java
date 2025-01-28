1c41c5e06112235748df0f9172b98618bc620be1
zooniaffreene <zooniaffreene1073@gmail.com>
2025-01-27 22:42:51 -0800

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,6 +1,17 @@
 public class InsertionSort implements Sorter {
 
+    @Override
     public void sort(int[] input) {
-        System.out.println("Insertion Sort!!!");
+        for (int i = 1; i < input.length; i++) {
+            int key = input[i];
+            int j = i - 1;
+
+           
+            while (j >= 0 && input[j] > key) {
+                input[j + 1] = input[j];
+                j--;
+            }
+            input[j + 1] = key;
+        }
     }
 }
