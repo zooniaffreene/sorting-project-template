ad736e823fc01cd59759e996bfacd60f40dd1b99
zooniaffreene <zooniaffreene1073@gmail.com>
2025-01-27 22:42:26 -0800

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,6 +1,17 @@
 public class SelectionSort implements Sorter {
 
+    @Override
     public void sort(int[] input) {
-        System.out.println("Selection Sort!!!");
+        for (int i = 0; i < input.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < input.length; j++) {
+                if (input[j] < input[minIndex]) {
+                    minIndex = j; 
+                }
+            }
+            int temp = input[minIndex];
+            input[minIndex] = input[i];
+            input[i] = temp;
+        }
     }
 }
